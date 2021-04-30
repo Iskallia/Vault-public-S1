7f95ddf3a5fcd3e67f6618656da4f3571aaf996e
BONNe <bonne@bonne.id.lv>
2021-04-30 16:46:30 +0300

    This change allows to see Mystery Stew Recipe in JEI and Crafting Books.
    Adds new tag `the_vault:gems` which contains:
    -  "the_vault:vault_rock"
    -  "the_vault:gem_alexandrite"
    -  "the_vault:gem_benitoite"
    -  "the_vault:gem_larimar"
    -  "the_vault:gem_black_opal"
    -  "the_vault:gem_painite"
    -  "the_vault:gem_iskallium"
    -  "the_vault:gem_renium"
    -  "the_vault:gem_gorginite"
    -  "the_vault:gem_sparkletine"
    -  "the_vault:gem_wutodie"
    -  "the_vault:gem_pog"
    
    Adjust mystery_stew recipe so it used tag `the_vualt:gems` and proper items.
    
    Part of #258

--- src/main/java/iskallia/vault/init/ModRecipes.java
+++ src/main/java/iskallia/vault/init/ModRecipes.java
@@ -1,7 +1,6 @@
 package iskallia.vault.init;
 
 import iskallia.vault.Vault;
-import iskallia.vault.recipe.MysteryStewRecipe;
 import iskallia.vault.recipe.RelicSetRecipe;
 import iskallia.vault.recipe.UnidentifiedRelicRecipe;
 import iskallia.vault.recipe.UpgradeCrystalRecipe;
@@ -14,13 +13,12 @@ public class ModRecipes {
 
 	public static class Serializer {
 		public static SpecialRecipeSerializer<RelicSetRecipe> CRAFTING_SPECIAL_RELIC_SET;
-		public static SpecialRecipeSerializer<MysteryStewRecipe> CRAFTING_SPECIAL_MYSTERY_STEW;
+		//public static SpecialRecipeSerializer<MysteryStewRecipe> CRAFTING_SPECIAL_MYSTERY_STEW;
 		public static SpecialRecipeSerializer<UnidentifiedRelicRecipe> CRAFTING_SPECIAL_UNIDENTIFIED_RELIC;
 		public static SpecialRecipeSerializer<UpgradeCrystalRecipe> CRAFTING_SPECIAL_UPGRADE_CRYSTAL;
 
 		public static void register(RegistryEvent.Register<IRecipeSerializer<?>> event) {
 			CRAFTING_SPECIAL_RELIC_SET = register(event, "crafting_special_relic_set", new SpecialRecipeSerializer<>(RelicSetRecipe::new));
-			CRAFTING_SPECIAL_MYSTERY_STEW = register(event, "crafting_special_mystery_stew", new SpecialRecipeSerializer<>(MysteryStewRecipe::new));
 			CRAFTING_SPECIAL_UNIDENTIFIED_RELIC = register(event, "crafting_special_unidentified_relic", new SpecialRecipeSerializer<>(UnidentifiedRelicRecipe::new));
 			CRAFTING_SPECIAL_UPGRADE_CRYSTAL = register(event, "crafting_special_upgrade_crystal", new SpecialRecipeSerializer<>(UpgradeCrystalRecipe::new));
 		}
